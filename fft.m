084b60a75bc376f9fbfc44490060bffe318dcb7e
CedricS789 <73127077+CedricS789@users.noreply.github.com>
2023-10-20 18:04:19 +0200

    Update fft.m
    adding the fi(t) plot

--- fft.m
+++ fft.m
@@ -1,30 +1,39 @@
-close all; clear; clc;
+close all;
+clear;
+clc;
 
-B = 200e6; %frequency range
-T = 0.4e-3; %chirp duration
-fs = 512e6; %sampling frequency
-fc = 100;  %carrier frequency
-beta = B/T;
- 
-duration = T; %signal duration
-t = 0:1/fs:(duration - 1/fs); %time vector
+B = 200e6;  % Frequency range
+T = 0.4e-3;  % Chirp duration
+fs = 512e6;  % Sampling frequency
+fc = 100;    % Carrier frequency
+beta = B / T;
+
+duration = T;  % Signal duration
+t = 0:1/fs:(duration - 1/fs);  % Time vector
 N_samples = length(t);
-frequencies = fs * (-N_samples/2:N_samples/2-1) / N_samples;  %frequencies vector
+frequencies = fs * (-N_samples/2:N_samples/2-1) / N_samples;  % Frequency vector
 
-y = cos(2*pi*fc*t + beta*pi*t.^2);
+fi = beta * t;
+y = cos(2 * pi * fc * t + beta * pi * t.^2);
 
 figure;
-subplot(2, 1, 1);  % Create two subplots vertically, this is the first one
+subplot(3, 1, 2);  % Create three subplots vertically, this is the first one
 plot(t, y);
 title('Time Domain Signal')
 xlabel('Time (s)')
 ylabel('Amplitude')
 grid on
 
+subplot(3, 1, 1);  % This is the second subplot
+plot(t, fi);
+title('fi')
+xlabel('Time (s)')
+ylabel('fi (Hz)')
+grid on
+
 Y = fft(y);
 
-% Create a new figure for the frequency domain plot
-subplot(2, 1, 2);  % This is the second subplot
+subplot(3, 1, 3);  % This is the third subplot
 plot(frequencies, abs(fftshift(Y)))
 title('Frequency Domain Spectrum')
 xlabel('Frequency (Hz)')
